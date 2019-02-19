6c6b0375bf9db3b0f3716810e0bc6e95a1e459e2
Thomas Tyack <thomastyack@aceik.com.au>
2019-02-19 22:41:13 +1100

    Vary by raw URL

---
+++ src/Foundation/Cache/code/Processor/ApplyVaryByRawUrlCaching.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+
+namespace Site.Foundation.Analytics.Processor
+{
+	public class ApplyVaryByRawUrlCaching : RenderRenderingProcessor
+	{
+		public override void Process(RenderRenderingArgs args)
+		{
+			if (args.Rendered) return;
+
+			if (HttpContext.Current == null || !args.Cacheable ||
+				args.Rendering.RenderingItem?.InnerItem == null) return;
+
+			var renderingItem = args.Rendering.RenderingItem.InnerItem;
+
+			if (renderingItem["VaryByRawUrl"] != "1") return;
+                                                              
+			args.CacheKey += "_#rawurl:" + HttpContext.Current.Request.RawUrl;
+		}
+	}
+}
